85c643aedb2568c511e1d91d9817f0d27539e5ae
super1207 <pengyao1210@gmail.com>
2020-05-16 23:00:05 +0800

    Create dmpic.py

---
+++ dmpic.py
@@ -0,0 +1,15 @@
+'''
+/*
+ * Copyright 2020 DMProject and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/super1207/DMProject/blob/master/LICENSE
+ */
+'''
+
+'''
+@Description: 大漠插件7.1904[图色]API部分的python实现
+@FilePath: dmpic.py
+'''
